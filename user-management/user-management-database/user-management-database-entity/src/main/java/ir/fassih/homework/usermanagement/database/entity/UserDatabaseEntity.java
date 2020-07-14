d218b5001d9886652cccf259c3afaa2ab3cf0c29
alireza.fassih <fassih.alireza@gmail.com>
2020-07-15 00:19:01 +0430

    add user database entity

--- user-management/user-management-database/user-management-database-entity/src/main/java/ir/fassih/homework/usermanagement/database/entity/UserDatabaseEntity.java
+++ user-management/user-management-database/user-management-database-entity/src/main/java/ir/fassih/homework/usermanagement/database/entity/UserDatabaseEntity.java
@@ -1,4 +1,20 @@
 package ir.fassih.homework.usermanagement.database.entity;
 
-public class UserDatabaseEntity {
+import ir.fassih.homework.core.database.entity.AbstractMultiTenantDatabaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Table(name = "USERS")
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class UserDatabaseEntity extends AbstractMultiTenantDatabaseEntity {
+
+    @Column(nullable = false, name = "EMAIL")
+    private String email;
+
 }
