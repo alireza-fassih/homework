ace89688461fc1368a35a237898f49e325fc196e
alireza.fassih <fassih.alireza@gmail.com>
2020-07-21 23:25:39 +0430

    add assertion to first test

--- user-management/user-management-database/user-management-database-tester/src/test/java/ir/fassih/homework/usermanagement/database/test/TestUserEntityExistOnDatabase.java
+++ user-management/user-management-database/user-management-database-tester/src/test/java/ir/fassih/homework/usermanagement/database/test/TestUserEntityExistOnDatabase.java
@@ -4,12 +4,16 @@ package ir.fassih.homework.usermanagement.database.test;
 import ir.fassih.homework.core.database.entity.MultiTenantId;
 import ir.fassih.homework.core.database.entity.TenantDatabaseEntity;
 import ir.fassih.homework.usermanagement.database.entity.UserDatabaseEntity;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -38,6 +42,14 @@ public class TestUserEntityExistOnDatabase {
         userDatabaseEntity.setId(id);
 
         entityManager.persist(userDatabaseEntity);
+
+
+        List<UserDatabaseEntity> allUsers = entityManager.createQuery("SELECT ude FROM UserDatabaseEntity ude", UserDatabaseEntity.class)
+                .getResultList();
+
+        Assert.assertEquals(1, allUsers.size());
+        Assert.assertEquals(userDatabaseEntity, allUsers.get(0));
+
     }
 
 }
