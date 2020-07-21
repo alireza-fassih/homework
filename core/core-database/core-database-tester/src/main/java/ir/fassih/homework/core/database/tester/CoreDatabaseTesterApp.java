37172682c64fdb885711921a35113b0496700565
alireza.fassih <fassih.alireza@gmail.com>
2020-07-21 23:44:50 +0430

    add tested module

---
+++ core/core-database/core-database-tester/src/main/java/ir/fassih/homework/core/database/tester/CoreDatabaseTesterApp.java
@@ -0,0 +1,14 @@
+package ir.fassih.homework.core.database.tester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreDatabaseTesterApp {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreDatabaseTesterApp.class, args);
+    }
+
+}
---
+++ core/core-database/core-database-tester/src/test/java/ir/fassih/homework/core/database/tester/TestTenantDatabaseEntity.java
@@ -0,0 +1,41 @@
+package ir.fassih.homework.core.database.tester;
+
+import ir.fassih.homework.core.database.entity.MultiTenantId;
+import ir.fassih.homework.core.database.entity.TenantDatabaseEntity;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
+class TestTenantDatabaseEntity {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    public void test() {
+        TenantDatabaseEntity tenantDatabaseEntity = new TenantDatabaseEntity();
+        tenantDatabaseEntity.setName("tenant 1");
+
+        entityManager.persist(tenantDatabaseEntity);
+
+
+        List<TenantDatabaseEntity> allTenants = entityManager.createQuery("SELECT tde FROM TenantDatabaseEntity tde", TenantDatabaseEntity.class)
+                .getResultList();
+
+        Assert.assertEquals(1, allTenants.size());
+        Assert.assertEquals(tenantDatabaseEntity, allTenants.get(0));
+
+    }
+
+}
